a5bbc848e4a86cdea3dd4de67c5acf868cf42712
MYousufSoomro <muhammad.yousuf1996@gmail.com>
2023-10-23 13:21:02 +0500

    React JS Basic Counter App using Hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>COUNT : {count}</h1>
+        <button onClick={increment}>Increment</button>
+        <br />
+        <button onClick={decrement}>Decrement</button>
       </header>
     </div>
   );
